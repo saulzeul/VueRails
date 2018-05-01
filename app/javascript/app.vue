6e5d006258034df052e328cba0434728016acfeb
Saul Espinoza <saul_zeul@hotmail.com>
2018-04-30 22:22:54 -0500

    Webpack y Servidor Rails no funcionaban
    ejecuto actualizacion
    yarn install
    npm install

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-    <b-navbar  fixed="top" toggleable="md" type="light" variant="light">
-        <b-navbar-brand class="nameEnterprice" href="#">
-          {{empresa}}
-        </b-navbar-brand>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item active>Acerca de</b-nav-item>
-        </b-navbar-nav>
-    </b-navbar>
     <p class="comentario">{{ message }}</p>
   </div>
 </template>
@@ -16,9 +8,7 @@
 export default {
   data: function () {
     return {
-      message: "Hola Saul Espinozaasa",
-      empresa: 'IPSL',
-      usuario: 'Saul Espinoza'
+      message: "Hola mundo desde VUE JS"
     }
   }
 }
@@ -29,8 +19,5 @@ export default {
   font-family: helvetica;
   font-size: 2em;
   text-align: center;
-}
-.nav{
-  background: #efefef
 }
 </style>
