f8f811cbed74f8c9d8196822143c9194b9d75d7e
Saul Espinoza <saul_zeul@hotmail.com>
2018-06-10 00:59:21 -0500

    Reparo rutas
    Reparo rutas de la vista login, forgot y panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'panel/panel'
-
-  get 'login/login'
-  get 'login/forgot'
-
   root to: 'login#login'
+  get '/forgot', to: 'login#forgot'
+  get '/panel', to: 'panel#panel'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
